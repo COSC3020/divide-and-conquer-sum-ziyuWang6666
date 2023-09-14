e538a1b529472e4def078ba3504390b532f6027f
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-09-13 21:06:19 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function divideAndConquerSum(a) {
-    return a;
+    if(a.length===0) return 0;
+    if(a.length===1) {
+        return a[0];
+    }
+    var mid = Math.round((a.length)/3);
+    var mid2 = mid * 2
+    
+    return divideAndConquerSum(a.slice(0,mid))+
+    divideAndConquerSum(a.slice(mid,mid2))+
+    divideAndConquerSum(a.slice(mid2))
 }
+
+var result = divideAndConquerSum([10,5,6,3,1,2]);
+console.log(result);
+// get help from TA: Ali
